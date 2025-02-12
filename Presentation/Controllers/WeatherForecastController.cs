b1f0998c57b0facc4055986950fc7ec342de38e8
Demian <demiangomes23@gmail.com>
2025-02-12 18:48:34 -0300

    Atualizar Swashbuckle.AspNetCore para a versão 7.2.0 e ajustar configurações do controlador e do pipeline de requisições

--- Presentation/Controllers/WeatherForecastController.cs
+++ Presentation/Controllers/WeatherForecastController.cs
@@ -5,21 +5,20 @@ using Microsoft.AspNetCore.Mvc;
 namespace MaisUmaRedeSocial.WebApi.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private readonly ILogger<WeatherForecastController> _logger;
     private readonly IWeatherForecastService _weatherForecastService;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger, IWeatherForecastService weatherForecastService)
+    public WeatherForecastController(IWeatherForecastService weatherForecastService)
     {
-        _logger = logger;
         _weatherForecastService = weatherForecastService;
     }
 
-    [HttpGet(Name = "GetWeatherForecast")]
+    [HttpGet]
     public IEnumerable<WeatherForecast> Get()
     {
         return _weatherForecastService.GetWeatherForecasts();
     }
 }
+
--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -15,10 +15,14 @@ builder.Services.AddScoped<IWeatherForecastService, WeatherForecastService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty; // Para acessar o Swagger na raiz (opcional)
+    });
 }
 
 app.UseHttpsRedirection();
